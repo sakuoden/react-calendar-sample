649dae9cc1ba112b48666dbbd71584a937c2ad1c
作佐部太祐 <sakusabudaisuke@sakusabudaisukenoMacBook-Pro.local>
2022-02-19 15:53:35 +0900

    ts-calendarを利用してカレンダーを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Calendar from 'ts-calendar/lib/model/calendar/Calendar';
+import CalendarWeek from 'ts-calendar/lib/model/calendar/CalendarWeek';
 
 const App = () => {
+  const [calendar, setCalendar] = useState<Calendar>(Calendar.ofThisMonth);
+
+  const nextMonth = () => {
+    setCalendar(calendar.nextMonth());
+  }
+
+  const lastMonth = () => {
+    setCalendar(calendar.lastMonth());
+  }
+
   return (
     <>
-      <div>テスト</div>
-      <h1>テスト</h1>
-      <h2>テスト2</h2>
-      <p>テストです</p>
+      <div>
+        <table>
+          <thead>
+          <tr>
+            <th>日</th>
+            <th>月</th>
+            <th>火</th>
+            <th>水</th>
+            <th>木</th>
+            <th>金</th>
+            <th>土</th>
+          </tr>
+          </thead>
+          <tbody className={"text-center align-top"}>
+          {calendar.getWeeks().map((week: CalendarWeek, index: number) => {
+            return (
+              <tr key={index}>
+                {week.getDates().map(date => {
+                  return (
+                    <td className={"border border-gray-500"} key={date.toString()}>
+                      {calendar.isThisMonth(date.toMonth()) ? (
+                        <>{ date.toNumber() }</>
+                      ) : (
+                        <></>
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
